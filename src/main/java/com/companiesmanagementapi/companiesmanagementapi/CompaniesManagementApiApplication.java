2a12308e4a1162c39e205e230cc25d9d4384dd9c
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-24 19:38:43 -0300

    Init project

---
+++ src/main/java/com/companiesmanagementapi/companiesmanagementapi/CompaniesManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.companiesmanagementapi.companiesmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaniesManagementApiApplication.class, args);
+	}
+}
