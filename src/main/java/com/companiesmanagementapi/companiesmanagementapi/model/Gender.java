57fc451aef0c1af3a2baf55c4fc659a9bcb01548
Gustavo Yamaguchi <gustavoyama@gmail.com>
2018-11-24 23:25:09 -0300

    Adding undercase names for Gender Enum

--- src/main/java/com/companiesmanagementapi/companiesmanagementapi/model/Gender.java
+++ src/main/java/com/companiesmanagementapi/companiesmanagementapi/model/Gender.java
@@ -1,5 +1,35 @@
 package com.companiesmanagementapi.companiesmanagementapi.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Gender {
-    MALE, FEMALE, OTHER
+
+    MALE("male"), FEMALE("female"), OTHER("other");
+
+    private String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+
+    @JsonValue
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return this.gender;
+    }
+
+    @JsonCreator
+    public static Gender fromText(String gender) {
+        for (Gender g: Gender.values()) {
+            if (g.getGender().equals(gender)) {
+                return g;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }
